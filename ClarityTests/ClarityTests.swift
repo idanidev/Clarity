200f3a2d64b059d581c03e6e848ef03f982f3dfe
daniel.benito.diaz <daniel.benito.diaz@artra.es>
2026-01-02 01:17:57 +0100

    Initial Commit

---
+++ ClarityTests/ClarityTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClarityTests.swift
+//  ClarityTests
+//
+//  Created by Daniel Benito Diaz on 2/1/26.
+//
+
+import Testing
+@testable import Clarity
+
+struct ClarityTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
